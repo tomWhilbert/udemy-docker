c771cde89a2ba354035aa5230e589dba42c8f1db
tomWhilbert <thomaswhibert@gmail.com>
2019-11-24 22:01:34 -0500

    add volumes to compose yaml

--- prod/frontend/src/App.js
+++ prod/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Bye There Again and Again
         </p>
         <a
           className="App-link"
