397d6cd3a4881920b3cb5da3b51f026a1ec01f7a
tomWhilbert <thomaswhibert@gmail.com>
2019-12-01 18:57:48 -0500

    add line to App.js

--- prod/frontend/src/App.js
+++ prod/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            HI There, Bye There
+            HI There, Wazzup, Bye There
         </p>
         <a
           className="App-link"
