b9ec98a7f85256652ffc810520aabaf32d15e1f8
tomWhilbert <thomaswhibert@gmail.com>
2019-12-08 22:06:41 -0500

    fib project begin setup

---
+++ complex/server/index.js
@@ -0,0 +1,33 @@
+const keys = require('./keys');
+
+// Expresss App Setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express ();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Postgres Client Setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+pgClient.on('error', () => console.log('Lost PG connection'));
+
+pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch(err => console.log(err));
+
+// Redis Client Setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+})
---
+++ complex/worker/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisHost: process.env.REDIS_PORT
+};
+
